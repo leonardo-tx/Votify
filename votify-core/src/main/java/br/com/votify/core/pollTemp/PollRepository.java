6938414c7b6b930685fc00c44b9f54fd81c06142
Andre Ferrarez <andre.augusto.17@gmail.com>
2025-03-12 21:59:49 -0300

    Implementacao da Feature de recuperacao de Poll (Enquete esecifica).

---
+++ votify-core/src/main/java/br/com/votify/core/pollTemp/PollRepository.java
@@ -0,0 +1,8 @@
+package br.com.votify.core.pollTemp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PollRepository extends JpaRepository<Poll, Long> {
+}
