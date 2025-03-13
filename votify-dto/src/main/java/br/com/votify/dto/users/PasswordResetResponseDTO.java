8faf12ff3ff3c3173a26d59e342bcb664f387698
Leonardo Teixeira <littledoge02@gmail.com>
2025-03-13 18:25:21 -0300

    :bug: Correção de validação no reset de senha

--- votify-dto/src/main/java/br/com/votify/dto/users/PasswordResetResponseDTO.java
+++ votify-dto/src/main/java/br/com/votify/dto/users/PasswordResetResponseDTO.java
@@ -1,14 +1,12 @@
 package br.com.votify.dto.users;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class PasswordResetResponseDTO {
     private String code;
     private int expirationMinutes;
