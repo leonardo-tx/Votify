1795ad2d41f9c10e516836e7ca5691ad0c73aa63
Leonardo Teixeira <littledoge02@gmail.com>
2025-04-03 14:05:16 -0300

    :wrench: Correção e ajustes de configurações

--- votify-api/src/main/resources/data-dev.sql
+++ votify-api/src/main/resources/data-dev.sql
@@ -12,35 +12,46 @@ VALUES
 
 -- Exemplo de dados adicionais para o ambiente de desenvolvimento
 -- Inserir algumas pesquisas de exemplo
-INSERT INTO TB_POLL (id, title, description, start_date, end_date, creator_id, poll_status)
+INSERT INTO TB_POLL (id, title, description, user_registration, choice_limit_per_user, start_date, end_date, responsible_id)
 VALUES
-    (1, 'Melhor Linguagem de Programação', 'Qual você considera a melhor linguagem de programação?', CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 1, 'ACTIVE'),
-    (2, 'Melhor Framework Web', 'Qual o melhor framework para desenvolvimento web?', CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 1, 'ACTIVE'),
-    (3, 'Metodologia Ágil Favorita', 'Qual sua metodologia ágil preferida?', CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 2, 'ACTIVE');
+    (1, 'Melhor Linguagem de Programação', 'Qual você considera a melhor linguagem de programação?', FALSE, 1, CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 1),
+    (2, 'Melhor Framework Web', 'Qual o melhor framework para desenvolvimento web?', TRUE, 1, CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 1),
+    (3, 'Metodologia Ágil Favorita', 'Qual sua metodologia ágil preferida?', FALSE, 1, CURRENT_TIMESTAMP(), DATEADD('DAY', 7, CURRENT_TIMESTAMP()), 2),
+    (4, 'Selecione seus jogos favoritos', 'Apenas o dessa lista, claro!\n Não dá pra colocar mais que 5 opções :P (Por favor devs, coloquem mais ;-;)', FALSE, 5, CURRENT_TIMESTAMP(), DATEADD('DAY', 14, CURRENT_TIMESTAMP()), 5);
 
 -- Inserir opções para as pesquisas
-INSERT INTO TB_POLL_OPTION (id, text, poll_id)
+INSERT INTO TB_VOTE_OPTION (poll_id, sequence, name, count)
 VALUES
-    (1, 'Java', 1),
-    (2, 'Python', 1),
-    (3, 'JavaScript', 1),
-    (4, 'C#', 1),
-    (5, 'Spring Boot', 2),
-    (6, 'React', 2),
-    (7, 'Angular', 2),
-    (8, 'Django', 2),
-    (9, 'Scrum', 3),
-    (10, 'Kanban', 3),
-    (11, 'XP', 3);
+    (1, 0, 'Java', 2),
+    (1, 1, 'Python', 0),
+    (1, 2, 'JavaScript', 1),
+    (1, 3, 'C#', 2),
+    (2, 0, 'Spring Boot', 1),
+    (2, 1, 'React', 1),
+    (2, 2, 'Angular', 1),
+    (2, 3, 'Django', 0),
+    (3, 0, 'Scrum', 1),
+    (3, 1, 'Kanban', 0),
+    (3, 2, 'XP', 0),
+    (4, 0, 'Hollow Knight', 3),
+    (4, 1, 'Hades', 2),
+    (4, 2, 'Baldur''s Gate 3', 3),
+    (4, 3, 'The Witcher 3', 2),
+    (4, 4, 'It Takes Two', 2);
 
 -- Inserir alguns votos
-INSERT INTO TB_VOTE (id, poll_id, user_id, option_id, vote_date)
+INSERT INTO TB_VOTE (poll_id, user_id, option_value)
 VALUES
-    (1, 1, 3, 1, CURRENT_TIMESTAMP()),
-    (2, 1, 4, 2, CURRENT_TIMESTAMP()),
-    (3, 1, 5, 3, CURRENT_TIMESTAMP()),
-    (4, 2, 3, 5, CURRENT_TIMESTAMP()),
-    (5, 2, 4, 6, CURRENT_TIMESTAMP()),
-    (6, 3, 3, 9, CURRENT_TIMESTAMP()),
-    (7, 3, 4, 9, CURRENT_TIMESTAMP()),
-    (8, 3, 5, 10, CURRENT_TIMESTAMP()); 
+    (1, 1, 1),
+    (1, 2, 8),
+    (1, 3, 4),
+    (1, 4, 8),
+    (1, 5, 1),
+    (2, 1, 2),
+    (2, 3, 1),
+    (2, 5, 4),
+    (3, 1, 1),
+    (4, 1, 31),
+    (4, 2, 3),
+    (4, 3, 4),
+    (4, 4, 29);
