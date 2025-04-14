248c3a4aa9231e3c0240096800a506f290c4fa46
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2025-04-14 15:17:56 -0300

    Feat/update user (#83)

--- votify-core/src/main/java/br/com/votify/core/service/UserService.java
+++ votify-core/src/main/java/br/com/votify/core/service/UserService.java
@@ -11,6 +11,7 @@ import br.com.votify.core.repository.UserRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -69,5 +70,71 @@ public class UserService {
 
     public void deleteUser(User user) {
         userRepository.delete(user);
+    }
+
+    @Transactional
+    public User updateUserInfo(String name, String userName, String email) throws VotifyException {
+        User user = context.getUserOrThrow();
+
+        if (name != null && !name.isBlank()) {
+            UserValidator.validateName(name);
+            user.setName(name);
+        }
+
+        if (userName != null && !userName.isBlank()) {
+            UserValidator.validateUserName(userName);
+            if (!user.getUserName().equals(userName) && userRepository.existsByUserName(userName)) {
+                throw new VotifyException(VotifyErrorCode.USER_NAME_ALREADY_EXISTS);
+            }
+            user.setUserName(userName);
+        }
+
+        if (email != null && !email.isBlank()) {
+            UserValidator.validateEmail(email);
+            if (!user.getEmail().equals(email) && userRepository.existsByEmail(email)) {
+                throw new VotifyException(VotifyErrorCode.EMAIL_ALREADY_EXISTS);
+            }
+
+            user.setEmail(email);
+        }
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void updateUserPassword(String oldPassword, String newPassword) throws VotifyException {
+        User user = context.getUserOrThrow();
+
+        if (!passwordEncoderService.checkPassword(user, oldPassword)) {
+            throw new VotifyException(VotifyErrorCode.INVALID_OLD_PASSWORD);
+        }
+
+        UserValidator.validatePassword(newPassword);
+        String encryptedNewPassword = passwordEncoderService.encryptPassword(newPassword);
+        user.setPassword(encryptedNewPassword);
+
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public User updateUserEmail(String email) throws VotifyException {
+        User user = context.getUserOrThrow();
+
+        if (email == null || email.isBlank()) {
+            throw new VotifyException(VotifyErrorCode.EMAIL_INVALID);
+        }
+
+        UserValidator.validateEmail(email);
+
+        if (!user.getEmail().equals(email)) {
+            if (userRepository.existsByEmail(email)) {
+                throw new VotifyException(VotifyErrorCode.EMAIL_ALREADY_EXISTS);
+            }
+            user.setEmail(email);
+        } else {
+            return user;
+        }
+
+        return userRepository.save(user);
     }
 }
---
+++ votify-dto/src/main/java/br/com/votify/dto/users/UserUpdateEmailRequestDTO.java
@@ -0,0 +1,12 @@
+package br.com.votify.dto.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateEmailRequestDTO {
+    private String email;
+} 
