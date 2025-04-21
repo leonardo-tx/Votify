e81ae1cc3d409b9f4205175141ed08b746103119
Victor Vianna <vitorvianna27@gmail.com>
2025-04-21 19:53:41 -0300

    :test_tube:test: Add new Tests

--- votify-test/src/test/java/br/com/votify/web/home/HomeTest.java
+++ votify-test/src/test/java/br/com/votify/web/home/HomeTest.java
@@ -4,6 +4,7 @@ import br.com.votify.web.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -11,6 +12,7 @@ import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeTest extends BaseTest {
     private HomePage page;
@@ -23,18 +25,39 @@ public class HomeTest extends BaseTest {
     void setupBeforeEach() {
         page = new HomePage(webDriver);
     }
-
+    
     @Test
-    public void checkPollListSize() {
-        
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+    public void testSearchForA() throws InterruptedException {
 
-        List<WebElement> pollCards = page.pollList.findElements(By.xpath("./div"));
-        assertEquals(0, pollCards.size());
+        WebElement searchInput = webDriver.findElement(By.id("nav-search-poll"));
+        searchInput.clear();
+        searchInput.sendKeys("a");
+        searchInput.sendKeys(Keys.ENTER);
+
+        Thread.sleep(1000);
+
+        List<WebElement> pollCards = page.pollList.findElements(By.xpath("./*"));
+        assertEquals(0, pollCards.size(), "Search for 'a' should return 0 polls");
+
+    }
+    
+    @Test
+    public void testSearchForEmptyString() throws InterruptedException {
+        WebElement searchInput = webDriver.findElement(By.id("nav-search-poll"));
+        searchInput.clear();
+        searchInput.sendKeys("a");
+        searchInput.sendKeys(Keys.ENTER);
+        searchInput.clear();
+        searchInput.sendKeys(" ");
+        searchInput.sendKeys(Keys.ENTER);
+
+        Thread.sleep(1000);
+        
+        WebElement errorMessage = webDriver.findElement(By.xpath("//div[contains(@class, 'bg-red-100')]"));
+        assertTrue(errorMessage.isDisplayed(), "Error message should be displayed");
+        assertEquals("Por favor, informe um título não vazio ou nulo para pesquisa.", 
+                errorMessage.getText().trim(), 
+                "Error message should match expected text");
     }
 
     // todo: Precisamos implementar alguns Polls para que possa ser possível testar a busca e inserção pela página.
