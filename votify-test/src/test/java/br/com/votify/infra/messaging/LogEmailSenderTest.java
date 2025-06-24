45277c83083ce07a862c0b796deb5fdcd515c116
Leonardo Teixeira <littledoge02@gmail.com>
2025-06-24 15:05:42 -0300

    test: Adiciona teste para LogEmailSenderTest

---
+++ votify-test/src/test/java/br/com/votify/infra/messaging/LogEmailSenderTest.java
@@ -0,0 +1,25 @@
+package br.com.votify.infra.messaging;
+
+import br.com.votify.core.model.user.User;
+import br.com.votify.core.model.user.field.Email;
+import br.com.votify.core.utils.exceptions.VotifyException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class LogEmailSenderTest {
+    @InjectMocks
+    private LogEmailSender logEmailSender;
+
+    @Test
+    void sendEmail() throws VotifyException {
+        User user = mock(User.class);
+        when(user.getEmail()).thenReturn(new Email("user@mail.com.br"));
+
+        logEmailSender.sendEmail(user, "Test Subject", "Test Message Body");
+    }
+}
