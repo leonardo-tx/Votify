419fd06d9bc279c551d942622e16b5382a82a90d
Leonardo Teixeira <littledoge02@gmail.com>
2025-06-25 17:19:17 -0300

    Ajeita mudanças feitas no front-end

--- votify-web/src/libs/api.ts
+++ votify-web/src/libs/api.ts
@@ -19,14 +19,14 @@ import UserUpdateEmailRequestDTO from "./users/UserUpdateEmailRequestDTO";
 
 export const api = axios.create({
   baseURL:
-      typeof window === "undefined"
-          ? `${process.env.NEXT_PROXY_URL ?? "http://localhost:8081"}/api`
-          : `${window.location.origin}/api`,
+    typeof window === "undefined"
+      ? `${process.env.NEXT_PROXY_URL ?? "http://localhost:8081"}/api`
+      : `${window.location.origin}/api`,
   withCredentials: true,
 });
 
 export const getUserById = async (
-    id: number,
+  id: number,
 ): Promise<ApiResponse<UserQueryView | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<UserQueryView>>(`/users/${id}`);
@@ -35,7 +35,7 @@ export const getUserById = async (
 };
 
 export const getCurrentUser = async (): Promise<
-    ApiResponse<UserDetailedView | null>
+  ApiResponse<UserDetailedView | null>
 > => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<UserDetailedView>>("/users/me");
@@ -51,19 +51,19 @@ export const deleteCurrentUser = async (): Promise<ApiResponse<null>> => {
 };
 
 export const updateUserInfo = async (
-    form: UserUpdateInfoDTO,
+  form: UserUpdateInfoDTO,
 ): Promise<ApiResponse<UserDetailedView | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.put<ApiResponse<UserDetailedView | null>>(
-        "/users/me/info",
-        form,
+      "/users/me/info",
+      form,
     );
     return data;
   });
 };
 
 export const updateUserEmail = async (
-    form: UserUpdateEmailRequestDTO,
+  form: UserUpdateEmailRequestDTO,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.put<ApiResponse<null>>("/users/me/email", form);
@@ -72,33 +72,33 @@ export const updateUserEmail = async (
 };
 
 export const updateUserPassword = async (
-    form: UserUpdatePasswordRequestDTO,
+  form: UserUpdatePasswordRequestDTO,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.put<ApiResponse<null>>(
-        "/users/me/password",
-        form,
+      "/users/me/password",
+      form,
     );
     return data;
   });
 };
 
 export const getUserByUserName = async (
-    userName: string,
-    cookie: string | undefined = undefined,
+  userName: string,
+  cookie: string | undefined = undefined,
 ): Promise<ApiResponse<UserQueryView | null>> => {
   return await commonRequester(async () => {
     if (cookie === undefined) {
       const { data } = await api.get<ApiResponse<UserDetailedView>>(
-          `/users/username/${userName}`,
+        `/users/username/${userName}`,
       );
       return data;
     }
     const { data } = await api.get<ApiResponse<UserDetailedView>>(
-        `/users/username/${userName}`,
-        {
-          headers: { cookie: cookie ?? "" },
-        },
+      `/users/username/${userName}`,
+      {
+        headers: { cookie: cookie ?? "" },
+      },
     );
     return data;
   });
@@ -112,176 +112,177 @@ export const logout = async (): Promise<ApiResponse<null>> => {
 };
 
 export const login = async (
-    credentials: UserLoginDTO,
+  credentials: UserLoginDTO,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<null>>(
-        "/auth/login",
-        credentials,
+      "/auth/login",
+      credentials,
     );
     return data;
   });
 };
 
 export const signup = async (
-    credentials: UserSignUpDTO,
+  credentials: UserSignUpDTO,
 ): Promise<ApiResponse<UserSignUpDTO | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<UserSignUpDTO>>(
-        "/auth/register",
-        credentials,
+      "/auth/register",
+      credentials,
     );
     return data;
   });
 };
 
 export const forgotPassword = async (
-    request: UserPasswordResetRequestDto,
+  request: UserPasswordResetRequestDto,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<null>>(
-        "/auth/forgot-password",
-        request,
+      "/auth/forgot-password",
+      request,
     );
     return data;
   });
 };
 
 export const resetPassword = async (
-    request: UserPasswordResetConfirmDTO,
+  request: UserPasswordResetConfirmDTO,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<null>>(
-        "/auth/reset-password",
-        request,
+      "/auth/reset-password",
+      request,
     );
     return data;
   });
 };
 
 export const confirmEmail = async (
-    request: EmailConfirmationRequestDTO,
+  request: EmailConfirmationRequestDTO,
 ): Promise<ApiResponse<null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<null>>(
-        `/auth/confirm-email`,
-        request,
+      `/auth/confirm-email`,
+      request,
     );
     return data;
   });
 };
 
 export const updatePoll = async (
-    id: number,
-    pollData: PollUpdateDTO,
+  id: number,
+  pollData: PollUpdateDTO,
 ): Promise<ApiResponse<PollSimpleView>> => {
   return await commonRequester(async () => {
     const { data } = await api.put<ApiResponse<PollSimpleView>>(
-        `/polls/${id}`,
-        pollData,
+      `/polls/${id}`,
+      pollData,
     );
     return data;
   });
 };
 
-export const cancelPoll = async (id: number): Promise<ApiResponse<any>> => {
-  const apiResponse = await api.delete<ApiResponse<any>>(`/polls/${id}/cancel`);
-  console.log(apiResponse);
-  return apiResponse.data;
+export const cancelPoll = async (id: number): Promise<ApiResponse<null>> => {
+  return await commonRequester(async () => {
+    const { data } = await api.delete<ApiResponse<any>>(`/polls/${id}/cancel`);
+    return data;
+  });
 };
 
 export const getMyPolls = async (
-    page: number = 0,
-    size: number = 10,
+  page: number = 0,
+  size: number = 10,
 ): Promise<ApiResponse<PageResponse<PollSimpleView> | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<PageResponse<PollSimpleView>>>(
-        `/polls/me?page=${page}&size=${size}`,
+      `/polls/me?page=${page}&size=${size}`,
     );
     return data;
   });
 };
 
 export const getPollsFromUser = async (
-    id: number,
-    page: number = 0,
-    size: number = 10,
+  id: number,
+  page: number = 0,
+  size: number = 10,
 ): Promise<ApiResponse<PageResponse<PollSimpleView> | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<PageResponse<PollSimpleView>>>(
-        `/polls/user/${id}?page=${page}&size=${size}`,
+      `/polls/user/${id}?page=${page}&size=${size}`,
     );
     return data;
   });
 };
 
 export const searchPollsByTitle = async (
-    title: string,
-    page: number = 0,
-    size: number = 10,
+  title: string,
+  page: number = 0,
+  size: number = 10,
 ): Promise<ApiResponse<PageResponse<PollSimpleView> | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<PageResponse<PollSimpleView>>>(
-        `/polls/search?title=${encodeURIComponent(title)}&page=${page}&size=${size}`,
+      `/polls/search?title=${encodeURIComponent(title)}&page=${page}&size=${size}`,
     );
     return data;
   });
 };
 
 export const getPollById = async (
-    id: number,
-    cookie: string | undefined = undefined,
+  id: number,
+  cookie: string | undefined = undefined,
 ): Promise<ApiResponse<PollDetailedView | null>> => {
   return await commonRequester(async () => {
     if (cookie === undefined) {
       const { data } = await api.get<ApiResponse<PollDetailedView>>(
-          `/polls/${id}`,
+        `/polls/${id}`,
       );
       return data;
     }
     const { data } = await api.get<ApiResponse<PollDetailedView>>(
-        `/polls/${id}`,
-        { headers: { cookie: cookie ?? "" } },
+      `/polls/${id}`,
+      { headers: { cookie: cookie ?? "" } },
     );
     return data;
   });
 };
 
 export const getAllActivePolls = async (
-    page: number = 0,
-    size: number = 10,
+  page: number = 0,
+  size: number = 10,
 ): Promise<ApiResponse<PageResponse<PollSimpleView> | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.get<ApiResponse<PageResponse<PollSimpleView>>>(
-        `/polls/active?page=${page}&size=${size}`,
+      `/polls/active?page=${page}&size=${size}`,
     );
     return data;
   });
 };
 
 export const vote = async (
-    id: number,
-    voteOption: VoteInsertDTO,
+  id: number,
+  voteOption: VoteInsertDTO,
 ): Promise<ApiResponse<number | null>> => {
   return await commonRequester(async () => {
     const { data } = await api.post<ApiResponse<number>>(
-        `/polls/${id}/vote`,
-        voteOption,
+      `/polls/${id}/vote`,
+      voteOption,
     );
     return data;
   });
 };
 
 const commonRequester = async <T>(
-    request: () => Promise<ApiResponse<T | null>>,
+  request: () => Promise<ApiResponse<T | null>>,
 ) => {
   try {
     return await request();
   } catch (error: any) {
     if (error.response === undefined) return getBackupErrorObject();
     if (
-        error.response.data.errorCode === VotifyErrorCode.ACCESS_TOKEN_EXPIRED ||
-        error.response.data.errorCode === VotifyErrorCode.COMMON_UNAUTHORIZED
+      error.response.data.errorCode === VotifyErrorCode.ACCESS_TOKEN_EXPIRED ||
+      error.response.data.errorCode === VotifyErrorCode.COMMON_UNAUTHORIZED
     ) {
       return await refreshAndDoRequestAgain(request);
     }
@@ -290,7 +291,7 @@ const commonRequester = async <T>(
 };
 
 const refreshAndDoRequestAgain = async <T>(
-    request: () => Promise<ApiResponse<T | null>>,
+  request: () => Promise<ApiResponse<T | null>>,
 ) => {
   try {
     await api.post<ApiResponse<null>>("/auth/refresh-tokens");
--- votify-web/src/pages/home/index.tsx
+++ votify-web/src/pages/home/index.tsx
@@ -8,96 +8,96 @@ import Head from "next/head";
 import { getAllActivePolls, getUserById } from "@/libs/api";
 
 interface Props {
-    polls: { poll: PollSimpleView; user: UserQueryView | null }[];
-    page: number;
-    totalPages: number;
+  polls: { poll: PollSimpleView; user: UserQueryView | null }[];
+  page: number;
+  totalPages: number;
 }
 
 const pageSize = 10;
 
 export default function Home({ polls, page, totalPages }: Props) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handlePageChange = (page: number) => {
-        router.push(
-            {
-                pathname: "/home",
-                query: {
-                    page: page,
-                },
-            },
-            undefined,
-            { shallow: false },
-        );
-    };
-
-    return (
-        <>
-            <Head>
-                <title>Home - Votify</title>
-            </Head>
-            <div className="flex flex-col gap-4">
-                <div className="flex flex-col gap-3">
-                    <PollList polls={polls} />
-                    <Pagination
-                        currentPage={page}
-                        totalPages={totalPages}
-                        onPageChange={handlePageChange}
-                    />
-                </div>
-            </div>
-        </>
+  const handlePageChange = (page: number) => {
+    router.push(
+      {
+        pathname: "/home",
+        query: {
+          page: page,
+        },
+      },
+      undefined,
+      { shallow: false },
     );
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Home - Votify</title>
+      </Head>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-3">
+          <PollList polls={polls} />
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (
-    context,
+  context,
 ) => {
-    const { page } = context.query;
-    const pageNumber = typeof page === "string" ? parseInt(page, 10) : 0;
+  const { page } = context.query;
+  const pageNumber = typeof page === "string" ? parseInt(page, 10) : 0;
 
-    const response = await getAllActivePolls(pageNumber, pageSize);
+  const response = await getAllActivePolls(pageNumber, pageSize);
 
-    if (!response.success) {
-        return {
-            props: {
-                polls: [],
-                page: pageNumber,
-                totalPages: 1,
-                errorMessage: "Não foi possível acesar as enquetes ativas",
-            },
-        };
-    }
-
-    let pollsWithUserData: {
-        poll: PollSimpleView;
-        user: UserQueryView | null;
-    }[] = [];
-
-    if (response.data && response.data.content) {
-        const users: Map<number, UserQueryView> = new Map();
-
-        pollsWithUserData = await Promise.all(
-            response.data.content.map(async (poll: PollSimpleView) => {
-                const userFromMap = users.get(poll.responsibleId);
-                if (userFromMap !== undefined) {
-                    return { poll, user: userFromMap };
-                }
-
-                const user = (await getUserById(poll.responsibleId)).data;
-                if (user !== null) {
-                    users.set(user.id, user);
-                }
-                return { poll, user };
-            }),
-        );
-    }
-
+  if (!response.success) {
     return {
-        props: {
-            polls: pollsWithUserData,
-            page: response.data?.pageNumber || pageNumber,
-            totalPages: response.data?.totalPages || 1,
-        },
+      props: {
+        polls: [],
+        page: pageNumber,
+        totalPages: 1,
+        errorMessage: "Não foi possível acesar as enquetes ativas",
+      },
     };
+  }
+
+  let pollsWithUserData: {
+    poll: PollSimpleView;
+    user: UserQueryView | null;
+  }[] = [];
+
+  if (response.data && response.data.content) {
+    const users: Map<number, UserQueryView> = new Map();
+
+    pollsWithUserData = await Promise.all(
+      response.data.content.map(async (poll: PollSimpleView) => {
+        const userFromMap = users.get(poll.responsibleId);
+        if (userFromMap !== undefined) {
+          return { poll, user: userFromMap };
+        }
+
+        const user = (await getUserById(poll.responsibleId)).data;
+        if (user !== null) {
+          users.set(user.id, user);
+        }
+        return { poll, user };
+      }),
+    );
+  }
+
+  return {
+    props: {
+      polls: pollsWithUserData,
+      page: response.data?.pageNumber || pageNumber,
+      totalPages: response.data?.totalPages || 1,
+    },
+  };
 };
